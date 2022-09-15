748a1e68be59c31ed20c7e4ee1fa20f2bfd0aa3b
vlad <vladmelnyk04@gmail.com>
2022-09-15 17:15:31 +0300

    conditional statements syntax for view files

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 $title = 'My blog';
-$numPosts = 0;
-$hasPosts = $numPosts = 5;
+$numPosts = 3;
+$hasPosts = $numPosts > 0;
 $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<?php
-if ($numPosts === 3) {
-    echo 'There are exactly 3 posts.';
-} elseif ($numPosts) {
-    echo 'Posts exists.';
-} else {
-    echo 'There are no posts.';
-}
-?>
+<?php if ($hasPosts): ?>
+    <?php if($numPosts === 3): ?>
+        There are exactly 3 posts.
+    <?php else: ?>
+        Posts exist.
+    <?php endif ?>
+<?php else: ?>
+    There are no posts
+<?php endif ?>
