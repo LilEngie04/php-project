4a500122eddada6755e76e4b84f3b7d23b06abe5
vlad <vladmelnyk04@gmail.com>
2022-09-15 17:08:42 +0300

    multiple conditions to execute alternate

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 $title = 'My blog';
-$numPosts = 10;
-$hasPosts = $numPosts > 0;
+$numPosts = 0;
+$hasPosts = $numPosts = 5;
 $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
 <?php
-if ($numPosts) {
+if ($numPosts === 3) {
+    echo 'There are exactly 3 posts.';
+} elseif ($numPosts) {
     echo 'Posts exists.';
+} else {
+    echo 'There are no posts.';
 }
 ?>
