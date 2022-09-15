3acd706a69c4d4599e3cef9cb13625ca3c3ec331
vlad <vladmelnyk04@gmail.com>
2022-09-15 17:23:06 +0300

    control structures into ternaries

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
 $title = 'My blog';
-$numPosts = 3;
+$numPosts = 0;
 $hasPosts = $numPosts > 0;
 $numPostsDisplay = "\"$numPosts\" posts";
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<?php if ($hasPosts): ?>
-    <?php if($numPosts === 3): ?>
-        There are exactly 3 posts.
-    <?php else: ?>
-        Posts exist.
-    <?php endif ?>
-<?php else: ?>
-    There are no posts
-<?php endif ?>
+<?= $hasPosts ? 'Posts exist.' : 'There are no posts.' ?>
