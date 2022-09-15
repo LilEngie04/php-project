1378ed7571019edf1c49be0680853c130a24373c
vlad <vladmelnyk04@gmail.com>
2022-09-15 20:22:18 +0300

    switch statement for advanced conditional logic

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 $title = 'My blog';
-$numPosts = 0;
+$numPosts = 10;
 $hasPosts = $numPosts > 0;
 $numPostsDisplay = "\"$numPosts\" posts";
+switch ($numPosts) {
+    case 0:
+        $message = 'There are no posts';
+        break;
+    case 1:
+    case 2:
+    case 3:
+        $message = 'There are a few posts';
+        break;
+    default:
+        $message = 'There are many posts.';
+}
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-<?= $hasPosts ? 'Posts exist.' : 'There are no posts.' ?>
+<p><?= $message ?></p>
+
