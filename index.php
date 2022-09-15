3aaaf95ae8e889801e0ab18f41c9eda9cf397001
vlad <vladmelnyk04@gmail.com>
2022-09-15 20:32:24 +0300

    display the contents of an array with a foreach loop

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ $title = 'My blog';
 $numPosts = 0;
 $hasPosts = $numPosts > 0;
 $numPostsDisplay = "\"$numPosts\" posts";
-$tags = [];
+$tags = ['php', 'docker', 'mysql'];
 ?>
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
-
-
+<?php foreach ($tags as $tag) {
+  echo "$tag<br/>";
+}
+?>
