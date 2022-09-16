d83c14bc33a71d470b2651b24430e1b91d9280a0
vlad <vladmelnyk04@gmail.com>
2022-09-16 10:02:00 +0300

    calling functions with return values in PHP

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 function getPosts()
 {
-
+    return [
+        [
+            'title' => 'How to learn PHP',
+            'content' => 'This is how you learn PHP',
+        ],
+        [
+            'title' => 'How to learn MySQL',
+            'content' => 'This is how you learn MySQL',
+        ],
+        [
+            'title' => 'How to learn Nginx',
+            'content' => 'This is how you learn Nginx',
+        ],
+    ];
 }
 
 $title = 'My blog';
-$posts = [
-        [
-        'title' => 'How to learn PHP',
-        'content' => 'This is how you learn PHP',
-        ],
-        [
-        'title' => 'How to learn MySQL',
-        'content' => 'This is how you learn MySQL',
-        ],
-        [
-        'title' => 'How to learn Nginx',
-        'content' => 'This is how you learn Nginx',
-        ],
-];
+$posts = getPosts();
 $numPosts = count($posts);
 $postText = $numPosts === 1 ? 'post' : 'posts';
 $numPostsDisplay = "$numPosts $postText";
@@ -26,11 +26,6 @@ $numPostsDisplay = "$numPosts $postText";
 <h1><?= $title ?></h1>
 <h2><?= $numPostsDisplay ?></h2>
 <?php for ($i = 0; $i < $numPosts; $i++) : ?>
-    <?php
-    if (str_contains($posts[$i]['title'], 'SQL')) :
-        break;
-    endif
-    ?>
     <h3><?= $posts[$i]['title'] ?></h3>
     <p><?= $posts[$i]['content'] ?></p>
 <?php endfor ?>
